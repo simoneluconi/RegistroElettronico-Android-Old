9f4b6711848c95597e6ef56061ec10164af2d305
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-25 14:44:48 +0200

    Switch to material intro
    * Allow sliding in the intro

--- app/src/main/java/com/sharpdroid/registroelettronico/Intro.java
+++ app/src/main/java/com/sharpdroid/registroelettronico/Intro.java
@@ -3,73 +3,67 @@ package com.sharpdroid.registroelettronico;
 import android.Manifest;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 
-import com.github.paolorotolo.appintro.AppIntro2;
-import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
-public class Intro extends AppIntro2 {
+public class Intro extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addSlide(AppIntroFragment.newInstance
-                (getString(R.string.benvenuto), "Verrai introdotto ad alcune funzionalità dell'app.",
-                        R.mipmap.ic_launcher,
-                        ContextCompat.getColor(this, R.color.colorPrimary)));
-        addSlide(AppIntroFragment.newInstance
-                ("Dettaglio medie", "Nei primi tre tab premi sulla scheda di una materia per visualizzare il dettaglio delle medie",
-                        R.drawable.intro1,
-                        ContextCompat.getColor(this, R.color.colorPrimary)));
-        addSlide(AppIntroFragment.newInstance
-                ("Media ipotetica", "oppure tieni premuto per visualizzare la media ipotetica",
-                        R.drawable.intro2,
-                        ContextCompat.getColor(this, R.color.colorPrimary)));
-        addSlide(AppIntroFragment.newInstance
-                ("Dettaglio voti", "Nel tab voti clicca su una scheda per visualizzare i commenti dei voti",
-                        R.drawable.intro3,
-                        ContextCompat.getColor(this, R.color.colorPrimary)));
-        addSlide(AppIntroFragment.newInstance
-                ("Agenda", "Nell'agenda le verifiche vengono indicate con dei pallini rossi, mentre i compiti con dei pallini gialli",
-                        R.drawable.intro4,
-                        ContextCompat.getColor(this, R.color.colorPrimary)));
-
+        addSlide(new SimpleSlide.Builder()
+                .title(getString(R.string.benvenuto))
+                .description("Verrai introdotto ad alcune funzionalità dell'app.")
+                .image(R.mipmap.ic_launcher)
+                .background(R.color.intro_blue)
+                .backgroundDark(R.color.intro_blue_dark)
+                .build());
+        addSlide(new SimpleSlide.Builder()
+                .title("Dettaglio medie")
+                .description("Nei primi tre tab premi sulla scheda di una materia per visualizzare il dettaglio delle medie")
+                .image(R.drawable.intro1)
+                .background(R.color.intro_blue)
+                .backgroundDark(R.color.intro_blue_dark)
+                .build());
+        addSlide(new SimpleSlide.Builder()
+                .title("Media ipotetica")
+                .description("oppure tieni premuto per visualizzare la media ipotetica")
+                .image(R.drawable.intro2)
+                .background(R.color.intro_blue)
+                .backgroundDark(R.color.intro_blue_dark)
+                .build());
+        addSlide(new SimpleSlide.Builder()
+                .title("Dettaglio voti")
+                .description("Nel tab voti clicca su una scheda per visualizzare i commenti dei voti")
+                .image(R.drawable.intro3)
+                .background(R.color.intro_blue)
+                .backgroundDark(R.color.intro_blue_dark)
+                .build());
+        addSlide(new SimpleSlide.Builder()
+                .title("Agenda")
+                .description("Nell'agenda le verifiche vengono indicate con dei pallini rossi, mentre i compiti con dei pallini gialli")
+                .image(R.drawable.intro4)
+                .background(R.color.intro_blue)
+                .backgroundDark(R.color.intro_blue_dark)
+                .build());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            addSlide(AppIntroFragment.newInstance
-                    ("Un ultima cosa", "Abbiamo bisogno dei permessi di archiviazione per scaricare le circolari e i file dalla didattica",
-                            R.drawable.download,
-                            ContextCompat.getColor(this, R.color.colorPrimary)));
-            askForPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 6);
+            addSlide(new SimpleSlide.Builder()
+                    .title("Un ultima cosa")
+                    .description("Abbiamo bisogno dei permessi di archiviazione per scaricare le circolari e i file dalla didattica")
+                    .image(R.drawable.download)
+                    .background(R.color.intro_blue)
+                    .backgroundDark(R.color.intro_blue_dark)
+                    .permissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE})
+                    .build());
         }
-        addSlide(AppIntroFragment.newInstance
-                ("Sei pronto!", "Ora sei pronto per utilizzare al meglio quest app. Ricorda, per qualsiasi problema contattaci a sharpdroidmail@gmail.com",
-                        R.drawable.done,
-                        ContextCompat.getColor(this, R.color.colorPrimary)));
-
-        setProgressButtonEnabled(true);
-    }
-
-    @Override
-    public void onSkipPressed(Fragment currentFragment) {
-        super.onSkipPressed(currentFragment);
-        ChiediPermessi();
-        finish();
-    }
-
-    @Override
-    public void onDonePressed(Fragment currentFragment) {
-        super.onDonePressed(currentFragment);
-        ChiediPermessi();
-        finish();
-    }
-
-    void ChiediPermessi() {
-        int result = 0;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            ActivityCompat.requestPermissions(Intro.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, result);
-        }
+        addSlide(new SimpleSlide.Builder()
+                .title("Sei pronto!")
+                .description("Ora sei pronto per utilizzare al meglio quest app. Ricorda, per qualsiasi problema contattaci a sharpdroidmail@gmail.com")
+                .image(R.drawable.done)
+                .background(R.color.intro_blue)
+                .backgroundDark(R.color.intro_blue_dark)
+                .build());
     }
 }
