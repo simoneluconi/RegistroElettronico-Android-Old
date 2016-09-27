13eac8be7280d5f28b5f2f9b895fab620c7008fb
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-09-27 18:22:21 +0200

    Add return void in gradle
    * Fix a warning

--- app/src/main/java/com/sharpdroid/registroelettronico/MainActivity.java
+++ app/src/main/java/com/sharpdroid/registroelettronico/MainActivity.java
@@ -50,7 +50,6 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -2216,7 +2215,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                     break;
                 }
-
             }
             return layout;
         }
